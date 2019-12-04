3b60c6d10013b3206af6caee0f93809a1a04ae86
Taufique Hussain <mth@indiana.edu>
2019-12-03 20:59:03 -0500

    Completed the code to make experimentable to some extent

--- alltoallv/test.c
+++ alltoallv/test.c
@@ -16,25 +16,49 @@ int main(int argc, char** argv){
     
     int param_1 = atoi(argv[1]); // Indicates total data volume
     int param_2 = atoi(argv[2]); // Indicates probability distribution which denotes how the data distribution would look like after all to all
+    int total_data_volume = param_1 * 1000000000;
+    int local_data_volume = (myrank < nprocs-1) ? (total_data_volume / nprocs) : (total_data_volume - (total_data_volume / nprocs) * myrank);
+
+    char* send_data = (char*) malloc(local_data_volume*sizeof(char));
 
     double p = (param_2*1.0)/100.0;
     double* distribution = (double*) malloc(nprocs*sizeof(double));
     int* sendcnt = (int*) malloc(nprocs*sizeof(int));
     int* sdispls = (int*) malloc(nprocs*sizeof(int));
-    int totsend = 0;
     for(int i = 0; i < nprocs; i++){
-        distribution[i] = pow(1.0 - p, i)*p;
-        sendcnt[i] = (int)(distribution[i] * param_1 * 1.0 * 1000000000);
-        totsend += sendcnt[i];
+        distribution[i] = pow((1.0 - p), i) * p;
+        sendcnt[i] = (int)(distribution[i] * local_data_volume);
         if(i == 0) sdispls[i] = 0;
-        else sdispls[i] = sdispls[i-1] + sendcnt[i];
+        else sdispls[i] = sdispls[i-1] + sendcnt[i-1];
     }
-    char* sendbuff = (char*) malloc(totsend*sizeof(int));
+
+    int* recvcnt = (int*) malloc(nprocs*sizeof(int));
+    int* rdispls = (int*) malloc(nprocs*sizeof(int));
+    MPI_Alltoall(sendcnt, 1, MPI_INT, recvcnt, 1, MPI_INT, MPI_COMM_WORLD);
+    rdispls[0] = 0;
+    for(int i = 1; i < nprocs; i++){
+        rdispls[i] = rdispls[i-1] + recvcnt[i-1];
+    }
+    int recv_data_volume = rdispls[nprocs-1] + recvcnt[nprocs-1];
+    char* recv_data = (char*) malloc(recv_data_volume*sizeof(char));
+    int count = 10;
+    double t0 = MPI_Wtime();
+    for(int i = 0; i < count; i++){
+        MPI_Alltoallv(send_data, sendcnt, sdispls, MPI_CHAR,
+                      recv_data, recvcnt, rdispls, MPI_CHAR, MPI_COMM_WORLD);
+    }
+    double t1 = MPI_Wtime();
+    double runtime = (t1-t0)/count;
     if(myrank == 0){
-        /*printf("nprocs:%d - Testing with data volume: %lf\n",nprocs, (param_1*1.0)/sum);*/
-        for(int i = 0; i < nprocs; i++){
-            printf("%d -> %d\n", sdispls[i], sendcnt[i]);
-        }
+        /*for(int i = 0; i < nprocs; i++){*/
+            /*printf("%10d\t%10d\n", sdispls[i], sendcnt[i]);*/
+        /*}*/
+        /*printf("========================================\n");*/
+        /*for(int i = 0; i < nprocs; i++){*/
+            /*printf("%10d\t%10d\n", rdispls[i], recvcnt[i]);*/
+        /*}*/
+        printf("nprocs: %d\tdistribution_param: %0.2lf\tdata_volume: %d GB\tcommunication_time: %0.10lf seconds\n",
+                nprocs, p, param_1, runtime);
     }
     MPI_Finalize();
     return 0;
