d5b10d79b79cc43d5103b660fb9043e71a4953fd
Taufique Hussain <mth@indiana.edu>
2019-12-07 16:00:02 -0800

    Added some more experiment script

---
+++ alltoallv/cori-256node.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+
+#SBATCH -q debug
+#SBATCH -C knl
+#SBATCH -A m1982
+#SBATCH -t 00:30:00
+
+#SBATCH -N 256
+#SBATCH -J alltoallv-cori-256nodes
+#SBATCH -o alltoallv-cori-256nodes.o%j
+
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 1 10
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 1 20
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 1 30
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 1 40
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 1 50
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 1 60
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 1 70
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 1 80
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 1 90
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 2 10
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 2 20
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 2 30
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 2 40
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 2 50
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 2 60
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 2 70
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 2 80
+srun -N 256 --ntasks-per-node 64 ./alltoallv-test 2 90
